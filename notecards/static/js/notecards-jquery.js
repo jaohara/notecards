6b4df581f66f518f67813bd8736921485fdf8adf
jaohara <johnoharaa@gmail.com>
2016-12-31 21:21:55 -0800

    Added and styled a basic login form, more little jquery bits.

--- notecards/static/js/notecards-jquery.js
+++ notecards/static/js/notecards-jquery.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $('#about-btn').click(function(event) {
-        alert("This button was triggered with jQuery");
+    /*
+        Defines jQuery behavior that spans the entire application.
+    */
+
+
+    /*
+        I want to have this better handled in the future. Instead of using
+        the browser confirm dialog, I want to make a div appear with a 
+        confirm or cancel button so we keep focus within the same window
+        and maintain a similar visual scheme between the app and the dialog.
+    */
+    $("a.delete-link").on("click", function(event){
+        event.stopImmediatePropagation();
+        event.preventDefault();
+
+        if(confirm("Really delete this deck?")){
+            window.location.href = $(this).attr("href");
+        }
     });
 });
